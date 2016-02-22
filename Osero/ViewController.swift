0c91ed5d87a94f2c3dba47f551d31ca7fa1f40ff
shima <z4623@gm.kochi-ct.jp>
2016-02-22 22:31:51 +0900

    Initial Commit

---
+++ Osero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Osero
+//
+//  Created by shima jinsei on 2016/02/22.
+//  Copyright © 2016年 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
